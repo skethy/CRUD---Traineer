5d3245b5bd236eb9ec025d01c9890b4fd5e62997
Silvio Willian <willianaika12@gmail.com>
2019-04-04 15:49:11 -0300

    Arquivos Modificados

---
+++ controllers/funcController.js
@@ -0,0 +1,89 @@
+const Func = require('../models/funcModels');
+
+const create = async (req, res) => {
+  try {
+    const {name, cpf, telefone, cep, numero, complemento} = req.body;
+
+    const func = new Func({
+      name,
+      cpf,
+      telefone,
+      cep,
+      numero,
+      complemento
+    });
+
+
+    await func.save();
+    return res.status(201).json({ message: 'Funcionário Criado!', func });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao Criar!'})
+  }
+}; 
+
+const findAll = async (req, res) => {
+  try {
+    const funcs = await Func.find({}); 
+    return res.status(200).json({ message: 'Funcionários Encontrados', funcs });
+  
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao Encontrar os Funcionarios!', error })
+  }
+};
+
+const findOne = async (req, res) => {
+  try {
+    const { cpf } = req.params;
+    const func = await Func.findOne({ cpf: cpf });
+      if (!func){
+      return res.status(404).json({ message: 'Funcionário Não Encontrado!' });
+    } 
+    return res.status(200).json({ message: 'Funcionario Encontrado', func });
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao Encontrar!', error })
+  }
+};
+
+
+/*Update de usuario. 
+  Atualização apenas dos parametros passados pelo body.
+*/
+const update = async (req, res) => {
+  try {
+    const { cpf } = req.params;
+    const infoFunc = req.body;
+    await Func.update({cpf: cpf},{$set: infoFunc})
+
+    const user = await Func.findOne({cpf:cpf}) 
+      if (!user) {
+        return res.status(404).json({ message: 'Funcionário Não Encontrado' });
+      }
+    res.status(200).send({'Funcionario Atualizado': user});
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro ao Atualizar!', error })
+  }
+};
+
+
+const remove = async (req, res) => {
+  try {
+    const { cpf } = req.params;
+    const func = await Func.findOneAndRemove({ cpf: cpf });
+    if (!func) {
+      return res.status(404).send('Funcionário Não Encontrado');
+    } 
+    return res.status(200).json('Funcionário Removido');
+  } catch (error) {
+    return res.status(500).send('Erro!')
+  }
+};
+
+
+
+module.exports = {
+  create,
+  findAll,
+  findOne,
+  update,
+  remove,
+};
--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-// require é usado para quando vamos chamar outro modulo da nossa aplicação
-// seja um modulo feito por nos, ou por outras pessoas
 const express = require('express'); 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const userRouter = require('./router/userRouter'); 
-const swaggerUi = require('swagger-ui-express');
+const userRouter = require('./router/funcRouter'); 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/treinamento'); // comando para conectar com o banco de dados
+mongoose.connect('mongodb://localhost:27017/funcionarios');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
-// body parser é modulo que vai passar de linha em linha da requisição enviado do front e ira montar objetos prontos para gente
-// esse objetos são o body, params e heady
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/hello', (req, res) => res.json('Olá Mundo!')); // rota de teste para ver se nossa aplicação esta realmente rodando
 app.use('/user', userRouter());
 
-const swaggerDocument = require('./docs/documentation.json');
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// swagger é o site que a gente viu de documentação e o a depencia que a gente chamou e usou de swaggerUi
-// é usada para exibir o documentação do mesmo jeito que é no site
-
-module.exports = app.listen(8000, () => console.log('Tá rodando')); 
-// app.linten() comando onde o backend ira realmente começar a rodar
-// e sera escutado a partir da porta 8000, localhost:8000/ 
-
+module.exports = app.listen(3000, () => console.log('Conexão Realizada!')); 
